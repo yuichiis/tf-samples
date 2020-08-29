4c64c8f28cea150fed5398c25eac178362c1d582
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2020-08-29 12:20:20 +0900

    update seq2seq-simple.py

--- samples/seq2seq-simple.py
+++ samples/seq2seq-simple.py
@@ -278,7 +278,7 @@ class Seq2seq(keras.Model):
         voc_id = self.start_voc_id;
         target_sentence =[]
         for i in range(input_length):
-            $in = $K->array([[$vocId]]);
+            inp = $K->array([[$vocId]]);
             [$predictions,$states] = $this->decoder->forward($in,$training=false,$states);
             $vocId = $K->argMax($predictions);
             $targetSentence[]=$vocId;
