56b00ca42fd53b53b8e24828f3aedfabab3c294b
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2020-08-29 09:31:36 +0900

    update seq2seq-simple.py

--- samples/seq2seq-simple.py
+++ samples/seq2seq-simple.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 from __future__ import absolute_import, division, print_function, unicode_literals
 import os
+import pickle
 #os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
 
 import keras
