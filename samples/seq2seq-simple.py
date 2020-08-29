2a60c3500e6d06ce17629192e0c924d1be9a9b48
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2020-08-29 10:54:22 +0900

    update seq2seq-simple.py

--- samples/seq2seq-simple.py
+++ samples/seq2seq-simple.py
@@ -256,7 +256,8 @@ class Seq2seq(keras.Model):
     ):
         with tf.GradientTape() as tape:
             outputs = self.forwardStep(inputs,trues,True)
-            self.loss_function(trues,outputs)
+            loss += self.loss_function(
+                trues,outputs)
 
     protected function backwardStep(NDArray $dout) : NDArray
     {
