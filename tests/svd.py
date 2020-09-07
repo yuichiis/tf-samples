54e25cca87ea1feefcd059a026dbeda67fa1b875
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2020-09-08 08:15:33 +0900

    update svd.py

--- tests/svd.py
+++ tests/svd.py
@@ -13,9 +13,9 @@ a=a.T
 s,u,vt = np.linalg.svd(a)
 
 np.set_printoptions(precision=2)
-print('----s----')
-print(s)
 print('----u----')
 print(u)
+print('----s----')
+print(s)
 print('----vt----')
 print(vt)
